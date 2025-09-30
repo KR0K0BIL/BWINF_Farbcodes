bd21b2ad832aee2205919dc1c160e102ae2877cb
KR0K0BIL <187011674+KR0K0BIL@users.noreply.github.com>
2025-09-30 17:48:30 +0200

    Readme, Endrunde Optionen, Ungültige Codes, css

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 # BWINF_Farbcodes
 
-Diese Webseite berechnet einen Farbcode für die Teilnahme am Bundeswettbewerb Informatik.
+Hier ausprobieren: https://kr0k0bil.github.io/BWINF_Farbcodes/
+
+Eine Webseite, die die Teilnahme am BWInf in bunte Kreise codiert, wie es in vielen Benutzernamen auf dem BWINF Discord Server zu sehen ist. Diese Codes können mit der Webseite auch decodiert werden!
+
+Beispiel (Trennstriche sind optional):\
+🟢🟤⚫|🟡⚪⚪\
+43. BWInf: 1. Runde 1. Preis, 2. Runde 3. Preis\
+44. BWInf: 1. Runde in Bearbeitung
 
 ⚫ - Ohne meine Teilnahme\
 ⚪ - Noch nicht gestartet\
 🟡 - In Bearbeitung\
 🔵 - Wird überprüft\
-🟢 - Erster Platz\
-🟠 - Zweiter Platz\
-🟤 - Dritter Platz\
+🟢 - Erster Preis\
+🟠 - Zweiter Preis\
+🟤 - Dritter Preis\
 🔴 - Kläglich Gescheitert
--- script.js
+++ script.js
@@ -6,28 +6,37 @@ optionen = "<option value='⚫'>⚫ - Ohne meine Teilnahme</option>\
             <option value='⚪'>⚪ - Noch nicht gestartet</option>\
             <option value='🟡'>🟡 - In Bearbeitung</option>\
             <option value='🔵'>🔵 - Wird überprüft</option>\
-            <option value='🟢'>🟢 - Erster Platz</option>\
-            <option value='🟠'>🟠 - Zweiter Platz</option>\
-            <option value='🟤'>🟤 - Dritter Platz</option>\
+            <option value='🟢'>🟢 - Erster Preis</option>\
+            <option value='🟠'>🟠 - Zweiter Preis</option>\
+            <option value='🟤'>🟤 - Dritter Preis</option>\
+            <option value='🔴'>🔴 - Kläglich Gescheitert</option>"
+
+optionen_endrunde = "<option value='⚫'>⚫ - Ohne meine Teilnahme</option>\
+            <option value='⚪'>⚪ - Noch nicht gestartet</option>\
+            <option value='🟡'>🟡 - In Bearbeitung</option>\
+            <option value='🔵'>🔵 - Wird überprüft</option>\
+            <option value='🟢'>🟢 - Bundessieger</option>\
+            <option value='🟠'>🟠 - Preisträger</option>\
+            <option value='🟤'>🟤 - Sonderpreis</option>\
             <option value='🔴'>🔴 - Kläglich Gescheitert</option>"
 
 window.addEventListener("load", (event) => {
     wettbewerbe_container = document.getElementById("wettbewerbe");
-    
+
     fuege_naechsten_wettbewerb_hinzu();
     fuege_naechsten_wettbewerb_hinzu();
     fuege_naechsten_wettbewerb_hinzu();
-    
-    document.getElementById("wettbewerb_" + aktueller_wettbewerb + "_runde_1").value="🟡";
-    document.getElementById("wettbewerb_" + aktueller_wettbewerb + "_runde_2").value="⚪";
-    document.getElementById("wettbewerb_" + aktueller_wettbewerb + "_runde_3").value="⚪";
-    
+
+    document.getElementById("wettbewerb_" + aktueller_wettbewerb + "_runde_1").value = "🟡";
+    document.getElementById("wettbewerb_" + aktueller_wettbewerb + "_runde_2").value = "⚪";
+    document.getElementById("wettbewerb_" + aktueller_wettbewerb + "_runde_3").value = "⚪";
+
     code_input = document.getElementById("code");
     code_input.addEventListener("enter", code_importieren);
     hinzufuegen_button = document.getElementById("hinzufuegen");
     hinzufuegen_button.addEventListener("click", fuege_naechsten_wettbewerb_hinzu);
     entfernen_button = document.getElementById("entfernen");
-    entfernen_button.addEventListener("click", entfernen_letzten_wettbewerb);
+    entfernen_button.addEventListener("click", entferne_letzten_wettbewerb);
     trennen_checkbox = document.getElementById("trennen");
     trennen_checkbox.addEventListener("change", ausgewaehlt);
     importieren_button = document.getElementById("importieren");
@@ -37,11 +46,12 @@ window.addEventListener("load", (event) => {
 });
 
 function fuege_naechsten_wettbewerb_hinzu() {
-    if (wettbewerb_nummer < 1) {return}
+    if (wettbewerb_nummer < 1) { return }
     wettbewerb_html = "<h2>" + wettbewerb_nummer + ". Bundeswettbewerb Informatik</h2>\
-        Runde 1: <select class='runde_select' id='wettbewerb_" + wettbewerb_nummer + "_runde_1' onchange='ausgewaehlt()'>" + optionen + "</select>\
-        Runde 2: <select class='runde_select' id='wettbewerb_" + wettbewerb_nummer + "_runde_2' onchange='ausgewaehlt()'>" + optionen + "</select>\
-        Runde 3: <select class='runde_select' id='wettbewerb_" + wettbewerb_nummer + "_runde_3' onchange='ausgewaehlt()'>" + optionen + "</select>";
+        <div class='runden'><div class='runde'>Runde 1: <select class='runde_select' id='wettbewerb_" + wettbewerb_nummer + "_runde_1' onchange='ausgewaehlt()'>" + optionen + "</select></div>\
+        <div class='runde'>Runde 2: <select class='runde_select' id='wettbewerb_" + wettbewerb_nummer + "_runde_2' onchange='ausgewaehlt()'>" + optionen + "</select></div>\
+        <div class='runde'>Runde 3: <select class='runde_select' id='wettbewerb_" + wettbewerb_nummer + "_runde_3' onchange='ausgewaehlt()'>" + optionen_endrunde + "</select></div></div>\
+        <div hidden class='ungueltig' id='wettbewerb_" + wettbewerb_nummer + "_ungueltig'>⚠️ Ungültige Auswahl</div>";
     wettbewerb_div = document.createElement('div');
     wettbewerb_div.id = "wettbewerb_" + wettbewerb_nummer;
     wettbewerb_div.innerHTML = wettbewerb_html;
@@ -49,8 +59,8 @@ function fuege_naechsten_wettbewerb_hinzu() {
     wettbewerb_nummer--;
 }
 
-function entfernen_letzten_wettbewerb() {
-    if (wettbewerb_nummer+2 > aktueller_wettbewerb) {return}
+function entferne_letzten_wettbewerb() {
+    if (wettbewerb_nummer + 2 > aktueller_wettbewerb) { return }
     wettbewerbe_container.firstChild.remove();
     wettbewerb_nummer++;
     ausgewaehlt();
@@ -59,10 +69,26 @@ function entfernen_letzten_wettbewerb() {
 function ausgewaehlt() {
     code = ""
     for (const wettbewerb_container of wettbewerbe_container.children) {
-        runde_1 = document.getElementById(wettbewerb_container.id + "_runde_1");
-        runde_2 = document.getElementById(wettbewerb_container.id + "_runde_2");
-        runde_3 = document.getElementById(wettbewerb_container.id + "_runde_3");
-        wettbewerb_code = runde_1.value + runde_2.value + runde_3.value
+        runde_1 = document.getElementById(wettbewerb_container.id + "_runde_1").value;
+        runde_2 = document.getElementById(wettbewerb_container.id + "_runde_2").value;
+        runde_3 = document.getElementById(wettbewerb_container.id + "_runde_3").value;
+        ungueltig = false;
+        if ((["⚪", "⚫"].includes(runde_1) && !["⚪", "⚫"].includes(runde_2))
+            || (["⚪", "⚫"].includes(runde_2) && !["⚪", "⚫"].includes(runde_3))) {
+            // Wenn man an einer Runde nicht teilnimmt, kann man auch nicht an der folgenden teilnehmen
+            ungueltig = true;
+        } else if ((["🟡", "🔵"].includes(runde_1) && !["⚪", "⚫", "🟡"].includes(runde_2))
+            || (["🟡", "🔵"].includes(runde_2) && !["⚪", "⚫"].includes(runde_3))) {
+            // Wenn Runde 1 läuft, kann Runde 2 noch nicht bewertet werden
+            ungueltig = true;
+        } else if ((!["🟢", "🟠"].includes(runde_1) && !["⚪", "⚫"].includes(runde_2))
+            || (!["🟢"].includes(runde_2) && !["⚪", "⚫"].includes(runde_3))) {
+            // Wenn man keinen 1. oder 2. Preis hat, kommt man nicht in die 2. Runde
+            // Ohne 1. Preis kommt man nicht in die 3. Runde
+            ungueltig = true;
+        }
+        document.getElementById(wettbewerb_container.id + "_ungueltig").hidden = !ungueltig;
+        wettbewerb_code = runde_1 + runde_2 + runde_3
         if (wettbewerb_code == "⚫⚫⚫" && code == "") {
             continue;
         }
@@ -76,11 +102,11 @@ function ausgewaehlt() {
 
 function code_importieren() {
     code = "|" + code_input.value;
-    i = code.length-1;
+    i = code.length - 1;
     wettbewerb_nummer_import = aktueller_wettbewerb;
     wettbewerb_nummer = aktueller_wettbewerb
     wettbewerbe_container.innerHTML = "";
-    ungueltig = false;
+    unvollstaendig = false;
     while (i > 1) {
         for (let runde = 3; runde > 0; runde--) {
             runde_code = "";
@@ -89,22 +115,23 @@ function code_importieren() {
                     runde_code = code[i];
                     i -= 1;
                     break
-                } else if (["🟡", "🔵", "🟢", "🟠", "🟤", "🔴"].includes(code[i-1] + code[i])) {
-                    runde_code = code[i-1] + code[i];
+                } else if (["🟡", "🔵", "🟢", "🟠", "🟤", "🔴"].includes(code[i - 1] + code[i])) {
+                    runde_code = code[i - 1] + code[i];
                     i -= 2;
                     break;
                 } else {
                     i -= 1;
                     if (i <= 0) {
                         if (runde == 3) {
+                            ausgewaehlt();
                             return;
                         }
-                        ungueltig = true;
+                        unvollstaendig = true;
                         break
                     }
                 }
             }
-            if (ungueltig) {
+            if (unvollstaendig) {
                 break;
             }
             if (runde == 3) {
@@ -112,7 +139,7 @@ function code_importieren() {
             }
             document.getElementById("wettbewerb_" + wettbewerb_nummer_import + "_runde_" + runde).value = runde_code;
         }
-        if (ungueltig) {
+        if (unvollstaendig) {
             break;
         }
         if (code[i] == '|') {
@@ -120,8 +147,9 @@ function code_importieren() {
         }
         wettbewerb_nummer_import--;
     }
-    if (ungueltig) {
-        alert("Ungültiger Code! Anzahl der Kreise muss durch 3 teilbar sein.")
+    if (unvollstaendig) {
+        entferne_letzten_wettbewerb();
+        alert("Unvollständiger Code! Anzahl der Kreise muss durch 3 teilbar sein.");
     }
     ausgewaehlt();
 }
